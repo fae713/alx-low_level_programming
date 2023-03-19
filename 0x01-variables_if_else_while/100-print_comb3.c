1cbf99f6feafb8492e8cfd8c25bb6a709b548aaa
fae713 <favourmichael47@gmail.com>
2023-03-18 23:25:47 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,7 +15,7 @@ int main(void)
 		for (n2 = n1 + 1; n2 < 10; n2++)
 		{
 			putchar((n1 % 10) + '0');
-			putchar((n1 % 10) + '0');
+			putchar((n2 % 10) + '0');
 
 			if (n1 == 8 && n2 == 9)
 				continue;
