608f2435c5b1fb51943e9048aa543d8af967d96f
fae713 <favourmichael47@gmail.com>
2023-03-21 12:21:26 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,11 +2,13 @@
 
 /**
  * jack bauer - Prints every minute of the day of Jack Bauer
+ *
  * Return: ...
  */
 
-void jack_bauer(void);
+void jack_bauer(void)
 {
+
 	int a, b, c, d;
 
 	for (a = 0; a <= 2; a++)
