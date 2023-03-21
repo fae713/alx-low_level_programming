325726d04434590ad41ef0913c436c47bcb1a494
fae713 <favourmichael47@gmail.com>
2023-03-21 11:38:38 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,13 @@
 int print_last_digit(int n)
 {
 	int last;
-
-	last = n %9;
-	{
+	
+	last = n % 10;
+	if (last < 0)
+       	{
 	last = last * -1;
 	}
 	_putchar(last + '0');
-	return (last);
+       	return (last);
 }
+	
