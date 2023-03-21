3ec3d810dbd7fa271dfa6c57f2795735735d2385
fae713 <favourmichael47@gmail.com>
2023-03-21 07:24:18 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+		_putchar(az);
+
+	_putchar('\n');
+}
