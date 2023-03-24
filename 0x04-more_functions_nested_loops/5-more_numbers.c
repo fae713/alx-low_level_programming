560ba1f49468f11d235a3e7fcb0f0e980dadff65
fae713 <favourmichael47@gmail.com>
2023-03-23 20:04:08 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * more_numbers - Print 10 times the numbers since 0 up to 14
@@ -22,6 +22,6 @@ void more_numbers(void)
 	}
 	_putchar((y % 10) + '0');
 	}
-	_putchar('\n')
+	_putchar('\n');
 	}
 }
