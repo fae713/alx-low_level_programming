00b78fcc89f004f3cf28cd7204dab3e2d72eefc6
fae713 <favourmichael47@gmail.com>
2023-03-23 19:57:45 -0700

    task 3

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,12 +2,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: The character to print
  * 
- * Return: 1 if true, 0 if false
- * On error, 1 return.
+ * Return1 if true, 0 if false
+ * On error, -1 return.
  */
-int_putchar(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
