d9781a8ecc6b301a3ac36268d8eee10a92930905
fae713 <favourmichael47@gmail.com>
2023-04-06 02:56:54 -0700

    task 0

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,13 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-	for (y = 0; y <= 14; y++)
+	for (y = 0; y <= 15; y++)
 	{
-	if (y > 0)
+	if (y >= 10)
 	{
-	_putchar((y / 10) + '0');
+	_putchar((y / 10) + 48);
 	}
-	_putchar((y % 10) + '0');
+	_putchar((y % 10) + 48);
 	}
 	_putchar('\n');
 	}
