220737bc80a23480377f4b5b7053356f7cc004be
fae713 <favourmichael47@gmail.com>
2023-07-03 10:49:23 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -6,16 +7,14 @@
  *
  *Return: the number of elements
 */
-
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t n = 0;
 
-		while (h)
+	while (h)
 	{
-		elements++;
+		n++;
 		h = h->next;
 	}
-
-	return (elements);
+	return (n);
 }
