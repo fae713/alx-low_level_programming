530cd009f7ca03222d93355b35b7a5171b1060e4
fae713 <favourmichael47@gmail.com>
2023-07-07 23:39:27 -0700

    task 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_
+#define _MAIN_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
