7af7e6355e3399847f595cf16d5a1b4ab525e11f
fae713 <favourmichael47@gmail.com>
2023-10-12 16:58:00 -0700

    task 1\2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,8 +2,8 @@
 
 /**
  * add_dnodeint - adds a new node at the beginning of a list
- * @head - the head of the new list
- * @n - the new node
+ * @head: the head of the new list
+ * @n: the new node
  *
  * Return: address of the new element or NULL
  */
