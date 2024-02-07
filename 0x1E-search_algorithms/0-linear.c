67aaa47522a02d044104979a884aaaec63431a01
fae713 <favourmichael47@gmail.com>
2024-02-07 00:01:56 -0800

    tasks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,11 +11,12 @@
  *
  *  Return: NULL or -1
  */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	for (i =  0; i < size; i++)
